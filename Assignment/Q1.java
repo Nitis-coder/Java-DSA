32c8b95069c3b8e35ad63f3d6fa60bc9b0fc3f2d
Nitish Thakur <kumarnitish95651@gmail.com>
2025-10-03 16:24:36 +0530

    Assignment

--- Assignment/Q1.java
+++ Assignment/Q1.java
@@ -1,5 +1,3 @@
-package Assignment;
-
 //Question 1: Given an integer array nums, return true if any value appears at least twice in 
 //the array, and return false if every element is distinct. 
 public class Q1 {
@@ -13,9 +11,8 @@ public class Q1 {
         }
         return false;
     }
-
    public static void main(String args[]){
        int nums[] = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
-       calls(nums);
+    System.out.println(  calls(nums));
     } 
 }

f92a3f62737bf72bf9e1306afeb1d126bc86db69
Nitish Thakur <kumarnitish95651@gmail.com>
2025-10-06 20:25:39 +0530

    Bubble Sorting

---
+++ 11-Basic Sorting Algorithms/Bubble_sort.java
@@ -0,0 +1,36 @@
+public class Bubble_sort {
+    public static void bubble(int arr[]){
+        for(int turn=0;turn<arr.length-1;turn++){
+            int sw=0;
+            for(int j=0;j<arr.length-1-turn;j++){
+                if(arr[j] > arr[j+1]){ //if 5>4 then;
+                   
+                    //Swiping  
+                    int temp=arr[j];   // t=5  
+                    arr[j]=arr[j+1];  //  5 = 4
+                    arr[j+1]=temp;   //   4 = 5
+
+                    sw++;
+
+                }
+               
+            }
+             
+             if(sw==0){
+                    break;
+                }
+           
+           System.out.println("sw"+sw);
+        }
+         
+         //  for Array printing.
+        // for(int i=0;i<arr.length;i++){
+        //     System.out.print(arr[i]+ " ");
+        // }
+    }
+    public static void main(String[] args) {
+        // int arr[]={5,4,1,3,2};
+        int arr[]={1,2,5,4};
+        bubble(arr);
+    }    
+}

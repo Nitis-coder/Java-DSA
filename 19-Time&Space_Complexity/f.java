a1c6a7548fa091a02d1637013ec2d80c7f7dae7c
Nitish Thakur <kumarnitish95651@gmail.com>
2026-09-09 01:06:48 +0530

    Time&complexity

---
+++ 19-Time&Space_Complexity/f.java
@@ -0,0 +1,11 @@
+public class f{
+    public static void main(String[] args) {
+        int arr[]= {1,2,3,4,5,6,7};
+        
+        System.out.println(arr.length);
+        int n= arr.length;
+        for(int i=0;i<=n-1;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
